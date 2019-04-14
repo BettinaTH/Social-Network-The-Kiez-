bfbcbcfe9d8441717585e78f9642676dd35d2cf5
BettinaThies <bettina.thies@outlook.de>
2019-04-14 18:33:35 +0200

    CSS for profile and friends

--- src/friends.js
+++ src/friends.js
@@ -8,30 +8,30 @@ class Friends extends React.Component {
     }
 componentDidMount(){
     this.props.dispatch(receiveFriendsWannabes());
-    // Disptach function from actions.js
 }
 
     render(){
-        console.log('props in  render Friendscomponent: ', this.props); // test show two object
         const besties = this.props.besties;
         const wannabes = this.props.wannabes;
-        console.log('const in render wannabes: ', wannabes);
-        console.log('const besties in render: ', besties);
         const myFriends = besties && besties.map((each)=>
-        <div key={each.id} className='friends'>
-            <img id='piclist' src={each.picture}></img>
-            <div className='eachName'>
-            <div>{each.first}</div> <div>{each.last}</div>
-            <button onClick={() => this.props.dispatch(deleteFriendship(each.id))}>END FRIENDSHIP</button>
+        <div><h3>Your friends</h3>
+            <div key={each.id} className='friends'>
+                <img id='piclist' src={each.picture}></img>
+                <div className='eachName'>
+                    <div>{each.first}</div> <div>{each.last}</div>
+                    <button onClick={() => this.props.dispatch(deleteFriendship(each.id))}>END FRIENDSHIP</button>
+                </div>
             </div>
         </div>
-    )
+            )
         const myWannabes = wannabes && wannabes.map((aspirant)=>
-        <div key={aspirant.id} className='friends'>
-            <img id='piclist' src={aspirant.picture}></img>
-            <div className='eachName'>
-            <div>{aspirant.first}</div> <div>{aspirant.last}</div>
-            <button onClick={() => this.props.dispatch(newFriend(aspirant.id))}>ACCEPT FRIEND REQUEST</button>
+        <div><h3>Your friend requests</h3>
+            <div key={aspirant.id} className='friends'>
+                <img id='piclist' src={aspirant.picture}></img>
+                <div className='eachName'>
+                    <div>{aspirant.first}</div> <div>{aspirant.last}</div>
+                    <button onClick={() => this.props.dispatch(newFriend(aspirant.id))}>ACCEPT FRIEND REQUEST</button>
+                </div>
             </div>
         </div>
         )
@@ -39,12 +39,10 @@ componentDidMount(){
             <div>
                 <div className='friends-container'>
                     <div className='friendslist'>
-                        <h3>Your friends</h3>
                         {myFriends}
                     </div>
                     <div className='friendslist'>
-                        <h3>Your friend requests</h3>
-                            {myWannabes}
+                        {myWannabes}
                     </div>
                 </div>
             </div>
